6726f1c3b0b71a71d0dbb7fa03d411185aefcd8c
HARDLINE\Anthony <lordrahvin@gmail.com>
2019-01-28 23:58:50 -0800

    Camera now zooms out when player moves faster and zooms in when moving slowly or stopped.
    Nobody ever reads this...?

--- Whisperdrift/Assets/Scripts/Player/PlayerController.cs
+++ Whisperdrift/Assets/Scripts/Player/PlayerController.cs
@@ -5,13 +5,18 @@ public class PlayerController : MonoBehaviour
 {
 	[SerializeField] private Engine engine = null;
 	[SerializeField] private float shotForce = 50f;
-
+	private Camera cam;
 	private Vector2 input;
 	private Rigidbody2D rb;
+	private float camSize=3;
+	const float camAdjustmentSpeed = 3f;
+	const float MIN_CAM_SIZE = 3f;
+	const float MAX_CAM_SIZE = 12f;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody2D>( );
+		cam = Camera.main;
 		Assert.IsNotNull( rb );
 		Assert.IsNotNull( engine );
 	}
@@ -26,6 +31,17 @@ public class PlayerController : MonoBehaviour
 
 		int sign = input.y > 0 ? 1 : -1;
 		engine.transform.localRotation = Quaternion.Euler( 0, 0, (Vector2.Angle( Vector2.right, input ) + 180) * sign );
+
+		AdjustCamera(5f + rb.velocity.magnitude);
+	}
+
+	private void AdjustCamera(float desiredCamSize) 
+	{
+		if ((desiredCamSize - camSize) > 0.1f) { camSize = camSize + camAdjustmentSpeed * Time.deltaTime; }
+		if ((camSize - desiredCamSize) > 0.1f) { camSize = camSize - camAdjustmentSpeed * Time.deltaTime; }
+		if (camSize < MIN_CAM_SIZE) { camSize = MIN_CAM_SIZE; }
+		if (camSize > MAX_CAM_SIZE) { camSize = MAX_CAM_SIZE; }
+		cam.orthographicSize = camSize;
 	}
 
 	public void MadeShot( Quaternion angle )
