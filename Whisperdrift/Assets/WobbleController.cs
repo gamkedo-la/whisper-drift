173d81d1c3c5c6000540fd7a0e2226d91ff4bd86
HARDLINE\Anthony <lordrahvin@gmail.com>
2019-01-29 20:47:59 -0800

    Added a small rotation effect to the camera, hoping for a more "whimsical" or dreamlike feel.
    "Careless, as a child with fire, so was I with time." -Terefi

---
+++ Whisperdrift/Assets/WobbleController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WobbleController : MonoBehaviour
+{
+    private const float WOBBLE_INTERVAL = 6f;
+	private const float WOBBLE_SPEED = 1f;
+	private const float MAX_WOBBLE_AMT = 10f;
+	private float goalWobble = 0f;
+	private float currentWobble = 0f;
+	private float timer = 4f;
+
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		timer = timer + Time.deltaTime;
+		if (timer > WOBBLE_INTERVAL) { SetNewWobble(); }
+
+		if (currentWobble < goalWobble)
+		{
+			currentWobble = currentWobble + (WOBBLE_SPEED * Time.deltaTime);
+			if (currentWobble > goalWobble) { currentWobble = goalWobble; }
+			transform.localRotation = Quaternion.Euler (transform.localRotation.x, transform.localRotation.y, currentWobble);
+		}
+		else if (currentWobble > goalWobble) 
+		{
+			currentWobble = currentWobble - (WOBBLE_SPEED * Time.deltaTime);
+			if (currentWobble < goalWobble) { currentWobble = goalWobble; }
+			transform.localRotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.y, currentWobble);
+		}
+		
+    }
+
+	private void SetNewWobble() 
+	{
+		goalWobble = Random.Range(-MAX_WOBBLE_AMT, MAX_WOBBLE_AMT);
+		timer = 0f;
+	}
+}
