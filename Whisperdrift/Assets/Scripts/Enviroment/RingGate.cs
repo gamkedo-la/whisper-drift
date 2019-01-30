9a963015be621aa35c11daccf3e4099542ca4fcb
KornelGM <KORNik_k@wp.pl>
2019-01-30 10:50:48 +0100

    First version of a Ring / Gate

---
+++ Whisperdrift/Assets/Scripts/Enviroment/RingGate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class RingGate : MonoBehaviour
+{
+	[SerializeField] private SpriteRenderer[] activationGraphics = null;
+	[SerializeField] private Material activationMaterial = null;
+
+	void Start ()
+	{
+		Assert.IsNotNull( activationGraphics );
+		Assert.AreNotEqual( activationGraphics.Length, 0 );
+
+		Assert.IsNotNull( activationMaterial );
+	}
+
+	private void OnTriggerEnter2D( Collider2D collision )
+	{
+		if ( !collision.gameObject.CompareTag( Tags.Player ) )
+			return;
+
+		foreach ( var sprite in activationGraphics )
+		{
+			sprite.material = activationMaterial;
+		}
+	}
+}
