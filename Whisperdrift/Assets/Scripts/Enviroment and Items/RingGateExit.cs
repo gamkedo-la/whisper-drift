ddb051746ece7a6d543f3d62a6d09abb05eb3b1b
KornelGM <KORNik_k@wp.pl>
2019-01-30 13:32:41 +0100

    Added 2 more Ring Gates and an Exit that is activeted after all the Rings, and takes us to the next level (currently the same level as we only have one)

--- Whisperdrift/Assets/Scripts/Enviroment and Items/RingGateExit.cs
+++ Whisperdrift/Assets/Scripts/Enviroment and Items/RingGateExit.cs
@@ -1,33 +1,45 @@
 ﻿using UnityEngine;
 using UnityEngine.Assertions;
 
-public class RingGate : MonoBehaviour
+public class RingGateExit : MonoBehaviour
 {
 	[SerializeField] private SpriteRenderer[] activationGraphics = null;
+	[SerializeField] private GameObject[] objectsToActivate = null;
 	[SerializeField] private Material activationMaterial = null;
 
 	private bool activated = false;
+	private bool poweredUp = false;
 
-	void Start ()
+	void Start( )
 	{
 		Assert.IsNotNull( activationGraphics );
 		Assert.AreNotEqual( activationGraphics.Length, 0 );
+		Assert.IsNotNull( objectsToActivate );
+		Assert.AreNotEqual( objectsToActivate.Length, 0 );
 
 		Assert.IsNotNull( activationMaterial );
 
-		LevelManger.Instance.AddRingGate( this );
+		LevelManger.Instance.AddRingGateExit( this );
 	}
 
 	private void OnTriggerEnter2D( Collider2D collision )
 	{
-		if ( !collision.gameObject.CompareTag( Tags.Player ) || !enabled || activated )
+		if ( !collision.gameObject.CompareTag( Tags.Player ) || !enabled || activated || !poweredUp )
 			return;
 
 		activated = true;
 
+		LevelManger.Instance.ExitActivated( );
+	}
+
+	public void PowerUp( )
+	{
+		poweredUp = true;
+
 		foreach ( var sprite in activationGraphics )
 			sprite.material = activationMaterial;
 
-		LevelManger.Instance.RingGateActiveted( this );
+		foreach ( var go in objectsToActivate )
+			go.SetActive(true);
 	}
 }
---
+++ Whisperdrift/Assets/Scripts/LevelManger.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+public class LevelManger : MonoBehaviour
+{
+	public static LevelManger Instance { get; private set; }
+
+	[SerializeField] private TextMeshProUGUI label = null;
+	[SerializeField] private string nextScene = "Main";
+	[SerializeField] private float levelChangeDelay = 1.0f;
+
+	private int activeRingGates = 0;
+	private List<RingGate> ringGates;
+	private RingGateExit ringGateExit;
+
+	private void Awake( )
+	{
+		ringGates = new List<RingGate>( );
+
+		if ( Instance != null && Instance != this )
+			Destroy( gameObject );
+		else
+			Instance = this;
+	}
+
+	private void OnDestroy( ) { if ( this == Instance ) { Instance = null; } }
+
+	void Start( )
+	{
+		Assert.IsNotNull( label );
+	}
+
+	public void AddRingGate( RingGate ringGate )
+	{
+		ringGates.Add( ringGate );
+		UpdateLabel( );
+	}
+
+	public void AddRingGateExit( RingGateExit ringGateExit )
+	{
+		this.ringGateExit = ringGateExit;
+	}
+
+	public void RingGateActiveted( RingGate ringGate )
+	{
+		activeRingGates++;
+		UpdateLabel( );
+
+		if ( activeRingGates == ringGates.Count )
+		{
+			ringGateExit.PowerUp( );
+			label.text = "Exit active";
+		}
+	}
+
+	public void ExitActivated( )
+	{
+		Invoke( "ChangeLevel", levelChangeDelay );
+	}
+
+	private void UpdateLabel( )
+	{
+		label.text = $"Rings active: {activeRingGates}/{ringGates.Count}";
+	}
+
+	private void ChangeLevel( )
+	{
+		SceneManager.LoadScene( nextScene );
+	}
+}
