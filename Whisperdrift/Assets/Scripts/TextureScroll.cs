77e4ba7382c2ad15341e312d6e9bb4e0d6230031
Terrence <t.w.mcdonnell10@gmail.com>
2019-01-29 17:20:10 -0700

    Add background and TextureScroll script

---
+++ Whisperdrift/Assets/Scripts/TextureScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScroll : MonoBehaviour
+{
+
+    // Scroll main texture based on time
+    public float scrollSpeed = 0.001f;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        Vector2 textureOffset = new Vector2(Time.time * scrollSpeed, 0);
+        rend.material.mainTextureOffset = textureOffset;
+    }
+}
