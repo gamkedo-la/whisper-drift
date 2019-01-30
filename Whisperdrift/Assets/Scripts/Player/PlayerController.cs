b461d6327762847203e7ec056d5b4d3ab605d1e1
HARDLINE\Anthony <lordrahvin@gmail.com>
2019-01-29 20:21:56 -0800

    Tweaked camera zoom to be more subtle.  Moved all the zoom code to the Camera object, which seems like a more sensible place.
    Hi Kornel!

--- Whisperdrift/Assets/Scripts/Player/PlayerController.cs
+++ Whisperdrift/Assets/Scripts/Player/PlayerController.cs
@@ -4,27 +4,24 @@ using UnityEngine.Assertions;
 public class PlayerController : MonoBehaviour
 {
 	[SerializeField] private float shotForce = 50f;
-	[SerializeField] private float camAdjustmentSpeed = 3f;
-	[SerializeField] private float camThreshold = 0.1f;
 
-	private Camera cam;
 	private Rigidbody2D rb;
-	private float camSize = 3f;
-
-	const float MIN_CAM_SIZE = 3f;
-	const float MAX_CAM_SIZE = 12f;
+	private ZoomController zoomController;
+	private float speedometer = 0f;
+	private float speedDelta = 0f;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody2D>( );
-		cam = Camera.main;
-
 		Assert.IsNotNull( rb );
+		zoomController = GameObject.FindObjectOfType<ZoomController>();
 	}
 
-	void Update ()
+	void FixedUpdate ()
 	{
-		//AdjustCamera(5f + rb.velocity.magnitude);
+		speedDelta = rb.velocity.magnitude - speedometer;
+		zoomController.Zoom(speedDelta);
+		speedometer = speedometer + speedDelta;
 	}
 
 	public void MadeShot( Quaternion angle )
@@ -32,12 +29,4 @@ public class PlayerController : MonoBehaviour
 		rb.AddForce( angle * Vector2.left * shotForce );
 	}
 
-	private void AdjustCamera( float desiredCamSize )
-	{
-		if ((desiredCamSize - camSize) > camThreshold ) { camSize = camSize + camAdjustmentSpeed * Time.deltaTime; }
-		if ((camSize - desiredCamSize) > camThreshold ) { camSize = camSize - camAdjustmentSpeed * Time.deltaTime; }
-		if (camSize < MIN_CAM_SIZE) { camSize = MIN_CAM_SIZE; }
-		if (camSize > MAX_CAM_SIZE) { camSize = MAX_CAM_SIZE; }
-		cam.orthographicSize = camSize;
-	}
 }
---
+++ Whisperdrift/Assets/Scripts/ZoomController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomController : MonoBehaviour
+{
+	private Camera cam = null;
+	private float zoomLevel = 5f;
+	private float tempZoomLevel = 5f;
+	private const float ZOOMLEVEL_CHANGE_SPEED = 0.5f;
+	private const float MIN_MOVEMENT_FOR_ZOOM_IN = .1f;
+	private const float MIN_MOVEMENT_FOR_ZOOM_OUT = .1f;
+	private const float MIN_ZOOM_LEVEL = 5f;
+	private const float MAX_ZOOM_LEVEL = 14f;
+	private const float ZOOM_FACTOR = 1f;
+
+
+
+	void Start()
+    {
+		cam = Camera.main;
+		cam.orthographicSize = zoomLevel;
+		zoomLevel = MIN_ZOOM_LEVEL;
+		tempZoomLevel = MIN_ZOOM_LEVEL;
+		SetCameraSize(tempZoomLevel);
+    }
+
+	private void FixedUpdate()
+	{
+		if (zoomLevel + MIN_MOVEMENT_FOR_ZOOM_OUT > tempZoomLevel) 
+		{
+			tempZoomLevel = tempZoomLevel + (ZOOMLEVEL_CHANGE_SPEED * Time.deltaTime);
+			if (tempZoomLevel > zoomLevel) { tempZoomLevel = zoomLevel; }
+			SetCameraSize(tempZoomLevel);
+		}
+		if (zoomLevel - MIN_MOVEMENT_FOR_ZOOM_IN < tempZoomLevel)
+		{
+			tempZoomLevel = tempZoomLevel - (ZOOMLEVEL_CHANGE_SPEED * Time.deltaTime);
+			if (tempZoomLevel < zoomLevel) { tempZoomLevel = zoomLevel; }
+			SetCameraSize(tempZoomLevel);
+		}
+
+	}
+
+	public void Zoom (float amount)
+	{
+		zoomLevel = Mathf.Clamp(zoomLevel + (amount * ZOOM_FACTOR), MIN_ZOOM_LEVEL, MAX_ZOOM_LEVEL);
+	}
+
+	private void SetCameraSize(float newSize) 
+	{
+		cam.orthographicSize = newSize;
+	}
+}
