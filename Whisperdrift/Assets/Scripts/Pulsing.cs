12c454b83d260d2f094b997a912870e473ffa1b2
KornelGM <KORNik_k@wp.pl>
2019-02-06 13:25:11 +0100

    Added script and test for pulsing material intensity

---
+++ Whisperdrift/Assets/Scripts/Pulsing.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Pulsing : MonoBehaviour
+{
+	[SerializeField] private Renderer objRenderer = null;
+	[SerializeField] private Color color = Color.white;
+	[SerializeField] private float minIntensily = 1f;
+	[SerializeField] private float maxIntensily = 3f;
+	[SerializeField] private float speed = 0.25f;
+
+	private Material material;
+
+	void Start ()
+	{
+		Assert.IsNotNull( objRenderer );
+		material = objRenderer.material;
+	}
+
+	void Update ()
+	{
+		float intensity = minIntensily + Mathf.PingPong( Time.time * speed, 1 ) * ( maxIntensily - minIntensily );
+		material.color = color * intensity;
+	}
+}
