c29f018eced4c99b0d380351a94844339cd320c3
HARDLINE\Anthony <lordrahvin@gmail.com>
2019-02-09 21:00:28 -0800

    Played with the fairies. :)
    “We don’t stop playing because we grow old; we grow old because we stop
    playing.” -George Bernard Shaw

--- Whisperdrift/Assets/Scripts/Fairy.cs
+++ Whisperdrift/Assets/Scripts/Fairy.cs
@@ -6,26 +6,30 @@ using UnityEngine.Assertions;
 public class Fairy : MonoBehaviour
 {
 
-	[SerializeField] private static int totalFariesFree = 0;
+	[SerializeField] private static int totalFairiesFree = 0;
+	[SerializeField] private int fairiesFree = 0;
 	[SerializeField] private Light mainLight = null;
 	[SerializeField] private SpriteRenderer mainSprite = null;
-	[SerializeField] private ParticleSystem particles = null;
+	[SerializeField] private ParticleSystem particleSys = null;
 	private Transform fairyHome = null;
 	[SerializeField] private float riseSpeed = 1f;
 	[SerializeField] private float playSpeed = 8f;
 	[SerializeField] private float fleeSpeed = 12f;
-	private float speed = 0f;
-	private const float PLAY_AREA = 1f;
-	private const float RISE = 4f;
-	private Vector3 destination;
-	private int playCount = 6;
-	private enum behavior { rise, play, flee};
+	[SerializeField] private float playRadius = 2f;
+	[SerializeField] private float speedVariation = 0.25f;
+	private enum behavior { rise, play, flee };
 	private behavior currentBehavior = behavior.rise;
+	private Vector3 destination;
+	private Vector3 playOrigin;
+	private int playCount = 6;
 	private float distanceToDestination = Mathf.Infinity;
-	private const float DISTANCE_THRESHOLD = 0.2f;
+	private float speed = 0f;
+	private const float RISE = 4f;
+	private const float DISTANCE_THRESHOLD = 0.4f;
 
 	void Start()
 	{
+		speedVariation = Random.Range(1f-speedVariation, 1f+speedVariation);
 		fairyHome = GameObject.FindGameObjectWithTag("FairyHome").GetComponent<Transform>();
 		Assert.IsNotNull(fairyHome);
 		transform.SetParent(fairyHome);
@@ -34,7 +38,7 @@ public class Fairy : MonoBehaviour
 
 		Assert.IsNotNull(mainLight);
 		Assert.IsNotNull(mainSprite);
-		Assert.IsNotNull(particles);
+		Assert.IsNotNull(particleSys);
 	}
 
 	void Update()
@@ -42,7 +46,7 @@ public class Fairy : MonoBehaviour
 		distanceToDestination = Vector3.Distance(destination, transform.position);
 		if (currentBehavior == behavior.rise && distanceToDestination <= DISTANCE_THRESHOLD) { StartPlaying(); }
 		if (currentBehavior == behavior.play && distanceToDestination <= DISTANCE_THRESHOLD) { PlayNext(); }
-		if (currentBehavior == behavior.flee && distanceToDestination <= DISTANCE_THRESHOLD) { DisableFairyObject(); }
+		//if (currentBehavior == behavior.flee && distanceToDestination <= DISTANCE_THRESHOLD) { DisableFairyObject(); }
 
 		transform.position += (destination - transform.position) * speed * Time.deltaTime;
 
@@ -52,36 +56,38 @@ public class Fairy : MonoBehaviour
 
 	private void Score() 
 	{
-		totalFariesFree += 1;
+		totalFairiesFree += 1;
+		fairiesFree = totalFairiesFree;
 	}
 
 	private void StartRising()
 	{
 		destination = transform.position + (Vector3.up * RISE);
-		speed = riseSpeed;
+		speed = riseSpeed * speedVariation;
 	}
 
 	private void StartPlaying() 
 	{
 		currentBehavior = behavior.play;
 		PlayNext();
-		speed = playSpeed;
+		speed = playSpeed * speedVariation;
+		playOrigin = transform.position;
 	}
 
 	private void StartFleeing() 
 	{
 		currentBehavior = behavior.flee;
 		destination = fairyHome.position;
-		speed = fleeSpeed;
+		speed = fleeSpeed * speedVariation;
 	}
 
 	private void PlayNext()
 	{
 		if (playCount <= 0) { StartFleeing(); return; }
 
-		float rndX = Random.Range(-PLAY_AREA, PLAY_AREA);
-		float rndY = Random.Range(-PLAY_AREA, PLAY_AREA);
-		destination = new Vector3(transform.position.x + rndX, transform.position.y + rndY, transform.position.z);
+		float rndX = Random.Range(-playRadius, playRadius);
+		float rndY = Random.Range(-playRadius, playRadius);
+		destination = new Vector3(playOrigin.x + rndX, playOrigin.y + rndY, playOrigin.z);
 		playCount = playCount - 1;
 	}
 
