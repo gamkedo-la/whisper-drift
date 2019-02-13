b5b063cb11109e2c611bdfe237fce0a3e3c85e79
KornelGM <KORNik_k@wp.pl>
2019-02-13 09:38:07 +0100

    Made sure you can't kill friends ;) And made prefab out of them :o

--- Whisperdrift/Assets/hopAround.cs
+++ Whisperdrift/Assets/hopAround.cs
@@ -1,31 +1,29 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class hopAround : MonoBehaviour
 {
-    public float jumpPower = 10f;
-    public float jumpRandomness = 10f;
-    public float jumpMinDelay = 1.0f;
-    public float jumpMaxDelay = 4.0f;
-    
-    private Rigidbody2D rb;        
+	public float jumpPower = 10f;
+	public float jumpRandomness = 10f;
+	public float jumpMinDelay = 1.0f;
+	public float jumpMaxDelay = 4.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        StartCoroutine(RandomJump());
-    }
+	private Rigidbody2D rb;
 
- 
-    IEnumerator RandomJump(){
-        while (true) {
-            Debug.Log("Jump!");
-            rb.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
-            rb.AddForce(transform.right * (jumpRandomness*2*Random.value-jumpRandomness), ForceMode2D.Impulse);
-            yield return new WaitForSeconds(Random.Range(jumpMinDelay,jumpMaxDelay));
-        }
-    }
+	void Start( )
+	{
+		rb = GetComponent<Rigidbody2D>( );
+		StartCoroutine( RandomJump( ) );
+	}
 
+	IEnumerator RandomJump( )
+	{
+		while ( true )
+		{
+			//Debug.Log("Jump!");
+			rb.AddForce( transform.up * jumpPower, ForceMode2D.Impulse );
+			rb.AddForce( transform.right * ( jumpRandomness * 2 * Random.value - jumpRandomness ), ForceMode2D.Impulse );
+			yield return new WaitForSeconds( Random.Range( jumpMinDelay, jumpMaxDelay ) );
+		}
+	}
 }
