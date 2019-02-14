4928b8d576e4e36253528aa2f8b8aba3f7efff12
KornelGM <KORNik_k@wp.pl>
2019-02-14 13:27:18 +0100

    Added animation and interaction to plants (and removed one that didn't work)

--- Whisperdrift/Assets/Scripts/Enviroment and Items/Plant.cs
+++ Whisperdrift/Assets/Scripts/Enviroment and Items/Plant.cs
@@ -3,13 +3,30 @@ using UnityEngine.Assertions;
 
 public class Plant : MonoBehaviour
 {
+	[SerializeField] private Animator animator = null;
+
 	void Start ()
 	{
-		//Assert.IsNotNull(  );
+		Assert.IsNotNull( animator );
+
+		Invoke( "StartIdle", Random.Range( 0.1f, 2.0f ) );
 	}
 
 	void Update ()
 	{
 
+	}
+
+	private void OnTriggerEnter2D( Collider2D collision )
+	{
+		if ( !collision.gameObject.CompareTag( Tags.Projectile ) && !collision.gameObject.CompareTag( Tags.Player ) )
+			return;
+
+		animator.SetTrigger( "Interacted" );
+	}
+
+	private void StartIdle()
+	{
+		animator.SetTrigger( "Idle" );
 	}
 }
