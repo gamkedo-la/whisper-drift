478dfd3512f10fce9a675789bef430497a34175e
KornelGM <KORNik_k@wp.pl>
2019-02-27 09:47:05 +0100

    Added a custom script for hittable objects so they only emit sound when velocity is greater then a min. (prevents emitting sounds during min. movements)

---
+++ Whisperdrift/Assets/Scripts/FMODPlayHitSound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class FMODPlayHitSound : MonoBehaviour
+{
+	[FMODUnity.EventRef] public string HitEvent;
+	public float MinVelocity = 1f;
+
+	private FMOD.Studio.EventInstance hit;
+
+	private void Start( )
+	{
+		hit = FMODUnity.RuntimeManager.CreateInstance( HitEvent );
+	}
+
+	private void OnCollisionEnter2D( Collision2D collision )
+	{
+		if (collision.relativeVelocity.magnitude >= MinVelocity)
+		{
+			hit.start( );
+		}
+	}
+}
