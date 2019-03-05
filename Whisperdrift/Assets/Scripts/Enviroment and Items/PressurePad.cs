16e987f1748dd91f5eaa4bf91fb6cb3d914871e1
stebssbets <32585154+stebssbets@users.noreply.github.com>
2019-03-05 01:29:54 -0600

    sliding door sound implementation WIP

--- Whisperdrift/Assets/Scripts/Enviroment and Items/PressurePad.cs
+++ Whisperdrift/Assets/Scripts/Enviroment and Items/PressurePad.cs
@@ -5,13 +5,27 @@ public class PressurePad : MonoBehaviour
 {
 	[SerializeField] private UnityEvent onPressed = null;
 	[SerializeField] private UnityEvent onReleased = null;
+    private FMOD.Studio.EventInstance slidingDoorSound;
+    private bool alreadyOpening;
 
-	private void OnTriggerEnter2D( Collider2D collision )
+    private void Awake()
+    {
+        alreadyOpening = false;
+        slidingDoorSound = FMODUnity.RuntimeManager.CreateInstance("event:/sliding_door_open_close");
+    }
+
+    private void OnTriggerEnter2D( Collider2D collision )
 	{
 		if ( !collision.gameObject.CompareTag( Tags.PressurePlate ) )
 			return;
 
 		onPressed.Invoke( );
+        if (!alreadyOpening)
+        {
+            slidingDoorSound.start();
+            alreadyOpening = true;
+        }
+
 	}
 
 	private void OnTriggerExit2D( Collider2D collision )
@@ -20,5 +34,7 @@ public class PressurePad : MonoBehaviour
 			return;
 
 		onReleased.Invoke( );
+        slidingDoorSound.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        alreadyOpening = false;
 	}
 }
