5ed1377b6c042a53624c35844a1aa0fa44a4dc27
stebssbets <32585154+stebssbets@users.noreply.github.com>
2019-03-05 14:48:02 -0600

    FMOD sound effects bus volume hooked up to master volume slider

--- Whisperdrift/Assets/Scripts/UI/VolumeControl.cs
+++ Whisperdrift/Assets/Scripts/UI/VolumeControl.cs
@@ -9,17 +9,22 @@ public class VolumeControl : MonoBehaviour
 	[SerializeField] private Slider volumeSlider = null;
 	[SerializeField] private string exposedPropertyName = "Volume";
 
+    FMOD.Studio.Bus SoundEffectsBus;
+
 	void Start( )
 	{
 		Assert.IsNotNull( audioMixer );
 		Assert.IsNotNull( volumeSlider );
 
 		volumeSlider.value = PlayerPrefs.GetFloat( exposedPropertyName, 1f );
+
+        SoundEffectsBus = FMODUnity.RuntimeManager.GetBus("bus:/sound_effects");
 	}
 
 	public void SetVolume( float value )
 	{
 		audioMixer.SetFloat( exposedPropertyName, Mathf.Log10( value ) * 20 );
 		PlayerPrefs.SetFloat( exposedPropertyName, value );
+        SoundEffectsBus.setVolume(value);
 	}
 }
