0b2ba9537bc07d6a5fb953549c02e1a79b8c4814
stebssbets <32585154+stebssbets@users.noreply.github.com>
2019-03-07 17:45:29 -0600

    added a script to the player to check for wall collisions and if so it plays the player_collides_wall_or_platform sound

---
+++ Whisperdrift/Assets/PlayerWallCollisionSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWallCollisionSound : MonoBehaviour
+{
+
+    FMOD.Studio.EventInstance PlayerCollidesNonDamagingObjectSound;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PlayerCollidesNonDamagingObjectSound = FMODUnity.RuntimeManager.CreateInstance("event:/player_collides_wall_or_platform");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag(Tags.Wall))
+            return;
+
+        PlayerCollidesNonDamagingObjectSound.start();
+    }
+}
