e24888f1a0075c91e7c370c21593b13b6e7c21ee
KornelGM <KORNik_k@wp.pl>
2019-03-08 09:15:56 +0100

    Player sound in Main Menu should be fixed now

---
+++ Whisperdrift/Assets/Scripts/EnableAfterDelay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class EnableAfterDelay : MonoBehaviour
+{
+	[SerializeField] private GameObject toEnable = null;
+	[SerializeField] private float delay = 1f;
+
+	void Start ()
+	{
+		Assert.IsNotNull( toEnable );
+
+		Invoke( "EnableNow", delay );
+	}
+
+	private void EnableNow ()
+	{
+		toEnable.SetActive( true );
+	}
+}
