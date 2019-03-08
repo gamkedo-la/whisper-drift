aed37af7160c66d540325e6aa11ed2ef2ebb0290
stebssbets <32585154+stebssbets@users.noreply.github.com>
2019-03-07 20:28:36 -0600

    player spawn sound added to FMOD list event and scripted in to the player spawn object, KNOWN BUG: when the title menu screen starts the player spawn is being called out of sync with the visual but it matches the visual in the game

---
+++ Whisperdrift/Assets/PlayerSpawnSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnSound : MonoBehaviour
+{
+    FMOD.Studio.EventInstance player_spawn_sound;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        player_spawn_sound = FMODUnity.RuntimeManager.CreateInstance("event:/player_spawn_sound");
+    }
+    void Start()
+    {
+        player_spawn_sound.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
