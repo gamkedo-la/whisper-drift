9dec5228a85089396787dc0b73cf617ccc23e87a
KornelGM <KORNik_k@wp.pl>
2019-03-09 13:00:56 +0100

    Reworked the cursor and added to all the scenes

--- Whisperdrift/Assets/Scripts/UI/CustomCursor.cs
+++ Whisperdrift/Assets/Scripts/UI/CustomCursor.cs
@@ -3,27 +3,32 @@ using UnityEngine.UI;
 
 public class CustomCursor : MonoBehaviour
 {
-	[SerializeField] private Image cursor = null;
-	[SerializeField] private Color clickColor = Color.white;
-	[SerializeField] private Color normalColor= Color.white;
+	[SerializeField] private GameObject cursorNormal = null;
+	[SerializeField] private GameObject cursorPressed = null;
 	[SerializeField] private Vector3 offset = Vector3.zero;
 
 	private void Start( )
 	{
 		Cursor.visible = false;
-
-		if ( cursor == null )
-			cursor = GetComponent<Image>( );
+		cursorNormal.SetActive( true );
+		cursorPressed.SetActive( false );
 	}
 
 	private void Update( )
 	{
-		transform.position = Input.mousePosition + offset;
+		cursorNormal.transform.position = Input.mousePosition + offset;
+		cursorPressed.transform.position = Input.mousePosition + offset;
 
 		if ( Input.GetMouseButtonDown( 0 ) )
-			cursor.color = clickColor;
+		{
+			cursorNormal.SetActive( false );
+			cursorPressed.SetActive( true );
+		}
 
 		if ( Input.GetMouseButtonUp( 0 ) )
-			cursor.color = normalColor;
+		{
+			cursorNormal.SetActive( true );
+			cursorPressed.SetActive( false );
+		}
 	}
 }
