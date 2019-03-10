8fbd880f32128eab0d8678b06dff6eecb5114f68
KornelGM <KORNik_k@wp.pl>
2019-03-10 17:36:09 +0100

    Updated and enhanced Credits screen

---
+++ Whisperdrift/Assets/Scripts/UI/CreditsControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CreditsControl : MonoBehaviour
+{
+	[SerializeField] private Transform creditText = null;
+	[SerializeField] private ScreenTransition screenTransition = null;
+	[SerializeField] private Animator animator = null;
+	[SerializeField] private float scrollSpeed = 5f;
+
+	void Start ()
+	{
+		Assert.IsNotNull( creditText );
+		Assert.IsNotNull( screenTransition );
+		Assert.IsNotNull( animator );
+	}
+
+	void Update ()
+	{
+		if ( Input.GetAxis("Vertical") != 0 )
+		{
+			if ( animator.enabled )
+			{
+				animator.StopPlayback( );
+				animator.enabled = false;
+			}
+
+			Vector2 pos = creditText.position;
+			pos.y += Input.GetAxis( "Vertical" ) * scrollSpeed * Time.deltaTime;
+			creditText.position = pos;
+		}
+	}
+
+	public void EndCredits()
+	{
+		screenTransition.StartTransition( );
+	}
+}
